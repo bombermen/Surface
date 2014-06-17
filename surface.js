1a9a6fba7aae542753c039854b65321af2958417
Jérémie <jferreira@hotmail.fr>
2014-06-17 21:29:26 +0200

    Lattice done

--- surface.js
+++ surface.js
@@ -3,7 +3,7 @@ var verts = new Array();
 var indices = new Array();
 
 function createSurface(path, shape) {
-    
+
     for (var i = 0; i < path.length; ++i) {
         createSectionAt(path, shape, i);
     }
@@ -66,72 +66,112 @@ function createSectionAt(path, shape, i) {
         s.y = shape[j].y / shapeCanvas.height - .5;
 
         var p = {x: 0, y: 0};
-         p.x = path[i].x / pathCanvas.width - .5;
-         p.y = path[i].y / pathCanvas.height - .5;
+        p.x = path[i].x / pathCanvas.width - .5;
+        p.y = path[i].y / pathCanvas.height - .5;
 
-        verts.push( p.x + s.x * v.x);
-        verts.push( p.y + s.x * v.y);
+        verts.push(p.x + s.x * v.x);
+        verts.push(p.y + s.x * v.y);
         verts.push(s.y);
     }
 }
 
 function generateLattice(shape, angle, steps) {
-    
+
+    var stepAngle = angle / steps;
+    var currentAngle = 0;
+
+    //generate sections
+    for (var i = 0; i < steps + 1; ++i) {
+        generateLatticeSection(shape, currentAngle);
+        currentAngle += stepAngle;
+    }
+
+    //triangulate
+    for (var i = 0; i < steps; ++i) {
+        for (var j = 0, len = shape.length - 1; j < len; ++j) {
+            //first triangle
+            indices.push(i * shape.length + j);
+            indices.push(i * shape.length + j + 1);
+            indices.push((i + 1) * shape.length + j);
+
+            //second triangle
+            indices.push(i * shape.length + j + 1);
+            indices.push((i + 1) * shape.length + j + 1);
+            indices.push((i + 1) * shape.length + j);
+        }
+    }
+}
+
+function generateLatticeSection(shape, angle) {
+
+    var cosT = Math.cos(angle);
+    var sinT = Math.sin(angle);
+
+    for (var i = 0, len = shape.length; i < len; ++i) {
+        var x = shape[i].x / shapeCanvas.width - .5;
+        var y = shape[i].y / shapeCanvas.height - .5;
+
+        verts.push(x * cosT);
+        verts.push(x * sinT);
+        verts.push(y);
+    }
 }
 
 function simpleExtrusion(shape, ratio, length) {
-    
-    for(var i = 0, len = shape.length; i < len; ++i) {
+
+    for (var i = 0, len = shape.length; i < len; ++i) {
         var s = {x: 0, y: 0};
         s.x = shape[i].x / shapeCanvas.width - .5;
         s.y = shape[i].y / shapeCanvas.height - .5;
-        
+
         verts.push(s.x);
         verts.push(s.y);
         verts.push(0);
     }
-    
-    for(var i = 0, len = shape.length; i < len; ++i) {
+
+    for (var i = 0, len = shape.length; i < len; ++i) {
         var s = {x: 0, y: 0};
         s.x = shape[i].x / shapeCanvas.width - .5;
         s.y = shape[i].y / shapeCanvas.height - .5;
-        
+
         verts.push(s.x / ratio);
         verts.push(s.y / ratio);
         verts.push(length);
-        
+
         indices.push(i);
         indices.push(i + 1);
         indices.push(len + i - 1);
-        
+
         indices.push(i + 1);
         indices.push(len + i);
         indices.push(len + i - 1);
     }
-    
+
     console.log(indices);
 }
 
 function generateSurface() {
-    
+
     verts = new Array();
     indices = new Array();
-    
+
     if ($("#bevel:checked").val() === 'on') {
         createSurface(pathCurve, shapeCurve);
     } else if ($("#simple:checked").val() === 'on') {
         simpleExtrusion(shapeCurve, .5, 1);
     } else if ($("#lattice:checked").val() === 'on') {
-        generateLattice(shapeCurve, 2 * Math.PI, 32);
+        generateLattice(shapeCurve, 1 * Math.PI, 32);
     }
+    console.log(verts);
+    console.log(indices);
     initBuffers(verts, indices);
 }
-$(document).ready(function () {
-	$("#editMode").hide();
-	$("#editModeLabel").hide();
+$(document).ready(function() {
+    $("#editMode").hide();
+    $("#editModeLabel").hide();
 
-	$("#generateSurface").click(function(){
-		$("#editMode").show();
-		$("#editModeLabel").show();
-	});
+    $("#generateSurface").click(function() {
+        $("#editMode").show();
+        $("#editModeLabel").show();
+    });
 });
